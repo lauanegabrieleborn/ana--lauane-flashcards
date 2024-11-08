865dde2012e2d7e17a9975cac8818b9d013d75c9
lauane <134061771+lauanegabrieleborn@users.noreply.github.com>
2024-11-08 15:56:37 -0300

    Add files via upload

--- ana--lauane-flashcards-main/ana--lauane-flashcards-23.10/app.js
+++ ana--lauane-flashcards-main/ana--lauane-flashcards-23.10/app.js
@@ -4,13 +4,15 @@ function criaCartao (categoria, pergunta, resposta) {
 cartao.className = 'cartao'
 }
 cartao.innerHTML = `
-<div class=”cartao__conteudo”>
+<div class="cartao__conteudo">
 <h3>${categoria}</h3>
-<div class=”cartao__conteudo__pergunta”>
-<p>${pergunta}</p>
+<h3>Programacao</h3>
+<div class="cartao__conteudo__pergunta">
+<p>O que e JavaScript?</p>
 </div>
-<div class=”cartao__conteudo__resposta”>
-<p>${resposta}</p>
+<div class="cartao__conteudo__resposta">
+<p>O JavaScript e uma linguagem de programacao</p>
 </div>
-</div>`
-container
+</div>
+`
+container.appendChild(cartao)
